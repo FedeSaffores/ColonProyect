ce172211ac992bf25c75520ad472e364d8829c30
federico.saffores <safforesfederico@gmail.com>
2024-05-31 11:19:27 -0300

    second commit

---
+++ backend-proyect/app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        
+        $token = $user->createToken('AuthToken')->plainTextToken;
+        return response()->json(['user' => $user, 'token' => $token], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if(Auth::attempt($credentials)){
+            $token = $request->user()->createToken('AuthToken')->plainTextToken;
+
+            return response()->json(['token' => $token,]);
+        }
+        return response()->json(['message' => 'Unauthorized'], 401);
+
+}
+public function logout(Request $request) 
+{
+    $request->user()->currentAccessToken()->delete();
+
+    return response()->json(['message' => 'Successfull logged out']);
+
+}
+}
